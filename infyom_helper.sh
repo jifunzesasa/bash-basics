5b9b2c5a723f11487327f4dd628704e78a8539ce
Alpha Olomi <alphaolomi@gmail.com>
2020-02-15 11:37:16 +0300

    Update infyom_helper.sh

--- infyom_helper.sh
+++ infyom_helper.sh
@@ -11,6 +11,6 @@ d1=${#dbs[@]}
 
 # use for loop to read all values and indexes
 for ((i = 1; i < ${m1} + 1; i++)); do
-    echo "${models[$i - 1]}"
+    # echo "${models[$i - 1]}"
     php artisan infyom:migration -n -q ${models[$i - 1]} --fieldsFile=/home/alpha/Documents/tantrade/new-tan/resources/model_schemas/${models[$i - 1]}.json
 done
